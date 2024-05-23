ef6e787805e2d13148eaad55491a125a70f95adf
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2024-05-23 11:17:35 +0200

    continued work on monitoring

--- app.py
+++ app.py
@@ -10,47 +10,52 @@ from src.utils import create_figure, prediction_from_model
 import flask_monitoringdashboard as dashboard # Importation de flask_monitoringdashboard pour monitorer l'utilisation de l'app web
 
 app = Flask(__name__)
+dashboard.config.init_from(file='config.cfg')
 dashboard.bind(app) # Mise en place du monitoring
 
+# Initialisation de l'objet GetData avec l'URL des données
 data_retriever = GetData(
     url="https://data.rennesmetropole.fr/api/explore/v2.1/catalog/datasets/etat-du-trafic-en-temps-reel/exports/json?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B"
 )
-data = data_retriever()
+data = data_retriever()  # Récupération des données
 
+# Chargement du modèle Keras
 model = load_model("model.h5")
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
+        # Création de la figure de la carte et conversion en JSON
         fig_map = create_figure(data)
         graph_json = fig_map.to_json()
 
+        # Récupération de l'heure sélectionnée par l'utilisateur
         selected_hour = request.form["hour"]
 
-        cat_predict = prediction_from_model(
-            model, selected_hour
-        )  # Ajouter selected_hour comme argument
+        # Prédiction à partir du modèle en utilisant l'heure sélectionnée
+        cat_predict = prediction_from_model(model, selected_hour)
 
+        # Définition des couleurs et des textes basés sur la prédiction
         color_pred_map = {
             0: ["Prédiction : Libre", "green"],
             1: ["Prédiction : Dense", "orange"],
             2: ["Prédiction : Bloqué", "red"],
         }
 
+        # Rendu du template avec les informations nécessaires
         return render_template(
-            "index.html", # Correction de home.html à index.html
+            "index.html",  # Correction du nom du template
             graph_json=graph_json,
             text_pred=color_pred_map[cat_predict][0],
             color_pred=color_pred_map[cat_predict][1],
         )
     else:
+        # Création de la figure de la carte pour les requêtes GET et conversion en JSON
         fig_map = create_figure(data)
-        graph_json = fig_map.to_json()  # Ajouter '()' pour appeler la méthode to_json
+        graph_json = fig_map.to_json()  # Appel de la méthode to_json()
 
-        return render_template("index.html", graph_json=graph_json) # Le fichier html n'est pas home.html
-
+        # Rendu du template avec la carte
+        return render_template("index.html", graph_json=graph_json)  # Correction du nom du template
 
 if __name__ == "__main__":
     app.run(debug=True)
